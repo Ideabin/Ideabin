62f998a6a9a9c4ecb837819fdf466c3f724bf6b2
Shadab Zafar <dufferzafar0@gmail.com>
2014-08-02 17:54:15 +0530

    Replace oursql by mysql-connector-python
    oursql depends on cython, both of which were having issues
    compiling
    
    The connector package seems to work for the entire team (and Travis)

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # The database that we'll be using for all testing purposes
 # Todo: What about production?
-SQLALCHEMY_DATABASE_URI = "mysql+oursql://user123:pass123@localhost/ideabin"
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector:" \
+                          "//user123:pass123@localhost/ideabin"
 DATABASE_CONNECT_OPTIONS = {}
