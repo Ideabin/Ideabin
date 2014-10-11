1b1f1d8d0f840ca2dfe0e276b643075fcf0fffe3
Shadab Zafar <dufferzafar0@gmail.com>
2014-10-11 14:02:17 +0530

    Improved frontend_create_app()
    The paths are taken by default.

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,17 +1,7 @@
 from flask import Flask
 
-import os
-
-frontend_folder = os.path.dirname(os.path.abspath(__file__))
-template_folder = os.path.join(frontend_folder, 'templates')
-static_folder = os.path.join(frontend_folder, 'static')
-
 def create_app():
-    app = Flask(__name__,
-                static_folder=static_folder,
-                template_folder=template_folder)
-
-    app.debug = True
+    app = Flask(__name__)
 
     # Configuration files
     app.config.from_object('config')
