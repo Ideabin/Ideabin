ebcf082628fb20e532b8597fdb7becbe88419c5a
TrigonaMinima <shivamrana95@yahoo.in>
2014-10-17 17:46:25 +0530

    Added a model for the tag-idea relationship

---
+++ server/tagging/models.py
@@ -0,0 +1,67 @@
+# Import the database object (db) from the main application module
+from misc import db
+
+# SQLAlchemy Exceptions
+from sqlalchemy import exc as SQLexc
+
+# UUID type for SQLAlchemy
+from misc.uuid import UUID
+import uuid
+
+# Required for timestamps
+import datetime as dt
+
+
+class Tagging(db.Model):
+    __tablename__ = 'tagging'
+
+    tag_id = db.Column(
+        UUID(), db.ForeignKey('tag.tag_id', ondelete='CASCADE', ),
+        nullable=False, primary_key=True, )
+    idea_id = db.Column(
+        UUID(), db.ForeignKey('idea.idea_id', ondelete='CASCADE'),
+        nullable=False, primary_key=True)
+
+    def __init__(self, tag_id, idea_id):
+        self.tag_id = tag_id
+        self.idea_id = idea_id
+
+    def __repr__(self):
+        return '<Tagging %r>' % self.tag_id
+
+    def new(tag_id, idea_id):
+        """
+        Add a new tag to an existing idea
+        """
+        new_tag = Tagging(tag_id, idea_id)
+        try:
+            db.session.add(new_tag)
+            db.session.commit()
+        except SQLexc.IntegrityError as e:
+            # Todo: (i)Raise a proper exception
+            # that the view will catch
+            # raise(e)
+            db.session.rollback()
+
+        new_tag.__repr__()
+
+        return new_tag
+
+    def delete(self):
+        """
+        Remove a tag from the database
+        """
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def json(self):
+        """
+        Return the tag's data in json form
+        """
+        json = {}
+        for prop, val in vars(self).items():
+            if not prop.startswith('_'):
+                json.update({prop: str(val)})
+        return json
