96b0a28295f08538b87aad3c43f8cd7f82eb5daf
Shadab Zafar <dufferzafar0@gmail.com>
2014-10-19 05:21:06 +0530

    Updated Views for Tags
    Creation now uses the Parser.
    
    GET view for both tag_id and tagname.
    
    DELETE view only for tagname. tag_id one can be added later.

--- server/tags/views.py
+++ server/tags/views.py
@@ -1,63 +1,64 @@
-# Our application and database
-from misc import db
-from server.exceptions import (
-    NotFound,
-    ServerError,
-    InvalidRequest,
-)
-
 import json
 
-# Some flask goodies
-from flask import make_response, jsonify, request, Blueprint
+from misc import db
+from server.exceptions import *
 
-# The model
+from flask import (
+    make_response,
+    jsonify,
+    request,
+    Blueprint
+)
+
+from misc.parser import Parser
+
 from .models import Tag
 
 tags_bp = Blueprint('tags', __name__)
 
 
-@tags_bp.route('/', endpoint='list_tags', methods=['GET'])
+@tags_bp.route('/', endpoint='list', methods=['GET'])
 def get_tags():
     """
     Sends a list of tags present in the database
     """
-    all_tags = Tag.query.all()
-    tags = []
-    if all_tags:
-        # Todo: (i)Add paging to retrieve next 50 tags and so on
-        # Todo: count of the ideas in a particular tag.
-        for tg in all_tags[0:50]:
-            tags.append(tg.json)
-    else:
+    # Todo: (1) Add paging to retrieve next 50 tags and so on
+    tags = Tag.query.limit(50)
+    if not tags:
         raise NotFound
 
-    resp = make_response(json.dumps(tags), 200)
+    resp = make_response(json.dumps([t.json for t in tags]), 200)
     resp.mimetype = 'application/json'
     return resp
 
 
-# @tags_bp.route('/<uuid:uid>', endpoint='list_tag_ideas', methods=['GET'])
-# def get_tag_ideas(uid):
-#     """
-#     Get all ideas with the matching tag_id
-#     """
+@tags_bp.route('/<uuid:tid>', endpoint='id', methods=['GET'])
+def get_tag(tid):
+    """
+    Get a specific tag with the matching tag_id
+    """
 
-#     s = Tagging.query.filter_by(tag_id=uid).all()
-#     print(str(t))
-#     if not s:
-#         NotFound
+    tag = Tag.query.filter_by(tag_id=tid).first()
+    if not tag:
+        raise NotFound
 
-#     sparks = []
-#     for spark in s:
-#         sparks.append(Idea.query.filter_by(idea_id=spark).first().json)
-
-#     resp = make_response(json.dumps(sparks), 200)
-#     resp.mimetype = 'application/json'
-#     return resp
+    return make_response(jsonify(tag.json), 200)
 
 
-@tags_bp.route('/', endpoint='create_tag', methods=['POST'])
+@tags_bp.route('/<string:tname>', endpoint='name', methods=['GET'])
+def get_tag(tname):
+    """
+    Get a specific tag with the matching tagname
+    """
+
+    tag = Tag.query.filter_by(tagname=tname).first()
+    if not tag:
+        raise NotFound
+
+    return make_response(jsonify(tag.json), 200)
+
+
+@tags_bp.route('/', endpoint='create', methods=['POST'])
 def create_tag():
     """
     Creates a new tag with the json data sent
@@ -65,27 +66,27 @@ def create_tag():
     if not request.json:
         raise InvalidRequest
 
-    t = Tag.new(
-        request.json['tagname'],
-        request.json['desc']
-    )
+    name = Parser.string('name', min=2, max=50)
+    desc = Parser.string('desc', optional=True)
 
-    return make_response(jsonify(t.json), 200)
+    new = Tag.new(name, desc)
+    return make_response(jsonify(new.json), 201)
 
 
-@tags_bp.route('/<uuid:tid>', endpoint='delete_tag', methods=['DELETE'])
-def delete_tag(uid):
+# Todo: Only moderators are allowed to delete a tag.
+@tags_bp.route('/<string:tname>', endpoint='delete', methods=['DELETE'])
+# @login_required()
+def delete_tag(tname):
     """
     Delete the tag with matching tag_id.
     """
 
-    tag = Tag.query.filter_by(tag_id=tid).first()
+    tag = Tag.query.filter_by(tagname=tname).first()
     if not tag:
         raise NotFound
 
-    tag = Tag.delete(tag)
+    Tag.delete(tag)
+    return make_response('', 204)
 
-    return make_response(jsonify(tag.json), 200)
-
-    # todo: edit the tag data (rename to the previous tags must be taken care
-    # of)
+# Todo: Edit the tag data
+# (rename to the previous tags must be taken care of)
