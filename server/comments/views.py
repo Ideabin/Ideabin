4b189dd825a13519bfbed596fab0f264e0e694c6
Shadab Zafar <dufferzafar0@gmail.com>
2014-10-25 09:02:41 +0530

    Updates to Comment API views
    Added Parser, Get Comment By Id function.
    
    @login_required in Post and Delete comment.

--- server/comments/views.py
+++ server/comments/views.py
@@ -10,7 +10,14 @@ from flask import (
     Blueprint
 )
 
-from .models import Comments
+from flask_login import (
+    current_user,
+    login_required
+)
+
+from misc.parser import Parser
+
+from .models import Comment
 from server.users.models import User
 from server.ideas.models import Idea
 
@@ -23,48 +30,61 @@ def get_comments(idea_id):
     """
     Returns a list of comments present in the database for the idea
     """
+
     # Todo: Add paging to retrieve next 50 comments and so on
-    comments = Comments.query.filter_by(idea_id=idea_id).limit(50)
+    comments = Comment.query.filter_by(idea_id=idea_id).limit(50)
 
     resp = make_response(json.dumps([c.json for c in comments]), 200)
     resp.mimetype = 'application/json'
     return resp
 
 
+@comments_bp.route('/<uuid:idea_id>/comments/<uuid:comment_id>', endpoint='id',
+                   methods=['GET'])
+def get_comment(idea_id, comment_id):
+    """
+    Get a specific comment with matching comment_id
+    """
+
+    comment = Comment.query.filter_by(
+        comment_id=comment_id, idea_id=idea_id).first()
+    if not comment:
+        raise NotFound
+
+    return make_response(jsonify(comment.json), 200)
+
+
 @comments_bp.route('/<uuid:idea_id>/comments/', endpoint='create',
                    methods=['POST'])
-# @login_required
+@login_required
 def create_comment(idea_id):
     """
     Creates a new comment with the json data sent
     """
+
     if not request.json:
         raise InvalidRequest
 
-    desc_md = Parser.string('desc')
+    desc = Parser.string('desc')
 
-    comment = Comments.new(user_id, idea_id, desc)
-    return make_response(jsonify(comment.json), 200)
+    comment = Comment.new(current_user.user_id, idea_id, desc)
+    return make_response(jsonify(comment.json), 201)
 
 
-@comments_bp.route('/<uuid:idea_id>/comments/', endpoint='delete',
-                   methods=['DELETE'])
-# @login_required
-def delete_comment(idea_id):
+@comments_bp.route('/<uuid:idea_id>/comments/<uuid:comment_id>',
+                   endpoint='delete', methods=['DELETE'])
+@login_required
+def delete_comment(idea_id, comment_id):
     """
     Delete the comment with matching comment_id
     """
-    if not request.json:
-        raise InvalidRequest
 
-    comment_id = Parser.uuid('comment_id')
-
-    comment = Comments.query.filter_by(comment_id, user_id, idea_id).first()
+    user_id = current_user.user_id
+    comment = Comment.query.filter_by(
+        comment_id=comment_id, user_id=user_id, idea_id=idea_id).first()
     if not comment:
         raise NotFound
     else:
-        Comments.delete(comment)
+        Comment.delete(comment)
 
     return make_response('', 204)
-
-# Todo: Update comment data
