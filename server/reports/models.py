1427af51dd5f2b4d848f1cdc7d3d28a563e43095
Shadab Zafar <dufferzafar0@gmail.com>
2014-11-04 22:49:15 +0530

    Work in progress

--- server/reports/models.py
+++ server/reports/models.py
@@ -1,28 +1,25 @@
-# Import the database object (db) from the main application module
 from misc import db
 
-# SQLAlchemy Exceptions
 from sqlalchemy import exc as SQLexc
 
-# UUID type for SQLAlchemy
 from misc.uuid import UUID
 import uuid
 
-# Required for timestamps
 import datetime as dt
 
 
 class Report(db.Model):
     __tablename__ = 'report'
 
-    # report_id =
-    # reported_on =
     user_id = db.Column(
-        UUID(), db.ForeignKey('user.user_id', ondelete='CASCADE', ),
-        nullable=False, primary_key=True, )
+        UUID(), db.ForeignKey('user.user_id', ondelete='CASCADE'),
+        nullable=False, primary_key=True)
     idea_id = db.Column(
         UUID(), db.ForeignKey('idea.idea_id', ondelete='CASCADE'),
         nullable=False, primary_key=True)
+
+    reported_on = db.Column(
+        db.DateTime, default=dt.datetime.utcnow(), nullable=False)
 
     def __init__(self, user_id, idea_id):
         self.user_id = user_id
@@ -30,16 +27,13 @@ class Report(db.Model):
 
     def new(user_id, idea_id):
         """
-        Reporting on an existing idea by an existing user.
+        Report an idea
         """
         report = Report(user_id, idea_id)
         try:
             db.session.add(report)
             db.session.commit()
         except SQLexc.IntegrityError as e:
-            # Todo: (i)Raise a proper exception
-            # that the view will catch
-            # raise(e)
             db.session.rollback()
         return True
 
